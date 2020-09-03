c90cf9b99c9dd61aac055b7044f3638f43b8d476
haidakto321 <kieusonhai07@gmail.com>
2020-09-03 14:19:45 +0700

    update login 1

--- src/main/java/com/codegym/meeting_room_management/dao/entity/MeetingRoomAsset.java
+++ src/main/java/com/codegym/meeting_room_management/dao/entity/MeetingRoomAsset.java
@@ -1,51 +1,51 @@
-package com.codegym.meeting_room_management.dao.entity;
-
-import javax.persistence.*;
-
-@Entity
-@Table(name = "meeting_room_assat")
-public class MeetingRoomAsset {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id_room")
-    private int idRoom;
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id_asset")
-    private int idAsset;
-    @Column(name = "quantity_user")
-    private int quantityUser;
-
-    public MeetingRoomAsset() {
-    }
-
-    public MeetingRoomAsset(int idRoom, int idAsset, int quantityUser) {
-        this.idRoom = idRoom;
-        this.idAsset = idAsset;
-        this.quantityUser = quantityUser;
-    }
-
-    public int getIdRoom() {
-        return idRoom;
-    }
-
-    public void setIdRoom(int idRoom) {
-        this.idRoom = idRoom;
-    }
-
-    public int getIdAsset() {
-        return idAsset;
-    }
-
-    public void setIdAsset(int idAsset) {
-        this.idAsset = idAsset;
-    }
-
-    public int getQuantityUser() {
-        return quantityUser;
-    }
-
-    public void setQuantityUser(int quantityUser) {
-        this.quantityUser = quantityUser;
-    }
-}
+//package com.codegym.meeting_room_management.dao.entity;
+//
+//import javax.persistence.*;
+//
+//@Entity
+//@Table(name = "meeting_room_assat")
+//public class MeetingRoomAsset {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    @Column(name = "id_room")
+//    private int idRoom;
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    @Column(name = "id_asset")
+//    private int idAsset;
+//    @Column(name = "quantity_user")
+//    private int quantityUser;
+//
+//    public MeetingRoomAsset() {
+//    }
+//
+//    public MeetingRoomAsset(int idRoom, int idAsset, int quantityUser) {
+//        this.idRoom = idRoom;
+//        this.idAsset = idAsset;
+//        this.quantityUser = quantityUser;
+//    }
+//
+//    public int getIdRoom() {
+//        return idRoom;
+//    }
+//
+//    public void setIdRoom(int idRoom) {
+//        this.idRoom = idRoom;
+//    }
+//
+//    public int getIdAsset() {
+//        return idAsset;
+//    }
+//
+//    public void setIdAsset(int idAsset) {
+//        this.idAsset = idAsset;
+//    }
+//
+//    public int getQuantityUser() {
+//        return quantityUser;
+//    }
+//
+//    public void setQuantityUser(int quantityUser) {
+//        this.quantityUser = quantityUser;
+//    }
+//}
---
+++ src/main/java/com/codegym/meeting_room_management/webservice/security/UserControllerHaiKS.java
@@ -0,0 +1,35 @@
+package com.codegym.meeting_room_management.webservice.security;
+
+import com.codegym.meeting_room_management.dao.DTO.AccountDTO;
+import com.codegym.meeting_room_management.dao.DTO.JwtResponse;
+import com.codegym.meeting_room_management.service.impl.UserServiceHaiKSImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*", exposedHeaders = "Authorization")
+@RequestMapping("")
+public class UserControllerHaiKS {
+    @Autowired
+    private AuthenticationManager authenticationManager;
+    @Autowired
+    private JwtTokenUtil jwtTokenUtil;
+    @Autowired
+    private UserServiceHaiKSImpl userServiceHaiKSImpl;
+
+    @PostMapping("/")
+    public ResponseEntity<?> login(@RequestBody AccountDTO accountDTO) {
+        System.out.println(accountDTO.getAccountPassword());
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(accountDTO.getAccountName(), accountDTO.getAccountPassword())
+        );
+        UserDetails userDetails = userServiceHaiKSImpl.loadUserByUsername(authentication.getName());
+        String jwtToken = jwtTokenUtil.generateToken(userDetails);
+        return ResponseEntity.ok(new JwtResponse(jwtToken, userDetails.getUsername(), userDetails.getAuthorities()));
+    }
+}
